ee913302d903ec125e6cc15825566d2567c72f96
Andrea Tomasi <tomasiandrea.at@gmail.com>
2019-02-08 00:36:08 +0100

    Renaming

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,4 +1,4 @@
-pub use data::Level;
+pub use state::Level;
 
 pub const SCREEN_WIDTH: f32 = 640.0;
 pub const SCREEN_HEIGHT: f32 = 480.0;
@@ -10,6 +10,6 @@ pub const BALL_SPEED: f32 = 100.;
 pub const PADDLE_SPEED: f32 = 6.;
 
 mod util;
-mod data;
+mod state;
 mod component;
 pub mod system;
